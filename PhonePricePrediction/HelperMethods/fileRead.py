55ff1428a0fc9ed6bf0240eff115ec682e709bf0
theladykiller <niki.plachetzky@gmail.com>
2025-04-27 19:37:46 +0200

    renamed column names

--- PhonePricePrediction/HelperMethods/fileRead.py
+++ PhonePricePrediction/HelperMethods/fileRead.py
@@ -14,4 +14,31 @@ def file_read():
     y_train = train_df["price_range"]  # Values 0, 1, 2, 3
     x_test = test_df.drop(columns=["id"])
 
+    # Conditionally rename columns
+    rename_conditions = {
+        'battery_power': 'Battery Power',
+        'blue': 'Bluetooth',
+        'clock_speed': 'Processor Clock Speed',
+        'dual_sim': 'Dual Sim',
+        'fc': 'Front Camera Mega Pixels',
+        'four_g': '4G',
+        'int_memory': 'Storage Capacity',
+        'm_dep': 'Phone Depth',
+        'mobile_wt': 'Weight',
+        'n_cores': 'Processor Cores',
+        'pc': 'Primary Camera Mega Pixels',
+        'px_height': 'Resolution Height',
+        'px_width': 'Resolution Width',
+        'ram': 'RAM',
+        'sc_h': 'Screen Height',
+        'sc_w': 'Screen Width',
+        'talk_time': 'Battery Duration',
+        'three_g': '3G',
+        'touch_screen': 'Touch Screen',
+        'wifi': 'WIFI'
+    }
+    x_train.rename(columns=rename_conditions, inplace=True)
+    x_test.rename(columns=rename_conditions, inplace=True)
+    y_train.name = 'Price Range'
+
     return x_train, y_train, x_test
